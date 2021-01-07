694b8926218b304112a66e761969a57fb547e49e
ddryo <dd.bocsfcryo@gmail.com>
2021-01-07 11:36:11 +0900

    タイムスタンプ処理変更

--- functions.php
+++ functions.php
@@ -16,6 +16,7 @@ define( 'ARKHE_CHILD_URI', get_stylesheet_directory_uri() );
  * style.css 読み込み
  */
 add_action( 'wp_enqueue_scripts', function() {
-	$time_stamp = date('mdgis');
+	// 開発時にブラウザキャッシュされないように、最終変更日時をクエリとして付与
+	$time_stamp = date( 'Ymdgis', filemtime( ARKHE_CHILD_PATH . '/style.css' ));
 	wp_enqueue_style( 'arkhe-child-style', ARKHE_CHILD_URI . '/style.css', [], $time_stamp );
 } );
